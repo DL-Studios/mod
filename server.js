15213b72eaf30a2a558de041d547be569756893f
louis <info@louisplayz.xyz>
2022-06-19 15:24:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -42,15 +42,3 @@ app.post('/apply', function(req, res){
         embeds: [embed]});
 })
 app.listen( function () { console.log(`http server:  Listening on port https://staffdlstudiosxyz.herokuapp.com/`) })
-
-
-
-
-
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-}, app
-)
-
-sslServer.listen( function () { console.log(`https server:  Listening on port https://staffdlstudiosxyz.herokuapp.com/`) })
